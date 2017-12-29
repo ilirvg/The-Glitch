6f270618eb5f0ef814afbbfa73587f40091014bb
Ilir V. Gruda <ilir_gruda@hotmail.com>
2017-12-29 10:15:06 +0100

    29/12

---
+++ The Glitch/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float startMenuTime;
+
+    private void Start()
+    {
+        Invoke("LoadNextLevel", startMenuTime);
+    }
+
+    public void LoadLevel(string name) {
+        Debug.Log("Level Load Requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+    public void LoadNextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+
+    public void QuickRequest() {
+        Debug.Log("Quit Request");
+        Application.Quit();
+    }
+}
